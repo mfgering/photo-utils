f5594d24cac5d40b50a67a3ade34ca28cfa8fd60
Mike Gering <mike@mfg-consulting-llc.com>
2019-10-11 08:39:17 -0400

    dedup

---
+++ imagededup-foo.py
@@ -0,0 +1,14 @@
+from imagededup.methods import PHash
+phasher = PHash()
+
+# Generate encodings for all images in an image directory
+encodings = phasher.encode_images(image_dir='images-dedup')
+
+# Find duplicates using the generated encodings
+duplicates = phasher.find_duplicates(encoding_map=encodings)
+
+# plot duplicates obtained for a given file using the duplicates dictionary
+from imagededup.utils import plot_duplicates
+plot_duplicates(image_dir='images-dedup',
+                duplicate_map=duplicates,
+                filename='images-dedup/dog.jpg')

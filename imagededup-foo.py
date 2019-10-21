eb77fce1b1604e55a3d2cb0bf46961416fa2915d
Mike Gering <mike@mfg-consulting-llc.com>
2019-10-21 08:52:51 -0400

    ?

--- imagededup-foo.py
+++ imagededup-foo.py
@@ -1,6 +1,14 @@
 from imagededup.methods import PHash
 import contextlib, hashlib, logging, os, sqlite3
 
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
+from matplotlib import figure
+from pathlib import Path, PosixPath
+from typing import Dict, Union, List
+
+import numpy as np
+from PIL import Image
 class DeDupe(object):
     def __init__(self):
         self.init_db()
@@ -83,9 +91,109 @@ class DeDupe(object):
                     result = cursor.execute(stmt, (image_id, encoding))
                     self.db_conn.commit()
 
+    def db2map(self, method="P"):
+        map = {}
+        with contextlib.closing(self.db_conn.cursor()) as cursor:
+            if method == "P":
+                for row in cursor.execute("SELECT * FROM p_encoding"):
+                    map[row[0]] = row[1]
+        return map
+
+    def find_duplicates(self, method="P"):
+        map = self.db2map(method)
+        d = self.p_hasher.find_duplicates(encoding_map=map, max_distance_threshold=10, scores=False)
+        reduced, set_seen = self.reduce_dups(d)
+        return reduced, set_seen
+
+    def reduce_dups(self, dups):
+        result = []
+        set_seen = set()
+        for id, dup_arr in dups.items():
+            if len(dup_arr) > 0 and id not in set_seen:
+                set_seen.add(id)
+                set_seen.update(dup_arr)
+                new_dups = [id]+dup_arr
+                result.append(new_dups)
+        return (result, set_seen)
+
+    def image_ids_2_filename(self, ids):
+        dct = {}
+        id_str = ','.join([str(i) for i in ids])
+        cursor = self.db_conn.execute("SELECT id, path, filename from image_info where id in (%s)" % id_str)
+        for id, path, filename in cursor:
+            dct[id] = os.path.join(path, filename)
+        return dct
+
+    def plot_images(
+        self,
+        image_dups,
+        id_map
+        #image_dir: PosixPath,
+        #orig: str,
+        #image_list: List,
+        #scores: bool = False,
+        #outfile: str = None,
+    ) -> None:
+        """
+        Plotting function for plot_duplicates() defined below.
+
+        Args:
+            image_dir: image directory where all files in duplicate_map are present.
+            orig: filename for which duplicates are to be plotted.
+            image_list: List of duplicate filenames, could also be with scores (filename, score).
+            scores: Whether only filenames are present in the image_list or scores as well.
+            outfile:  Name of the file to save the plot.
+        """
+        nrows = len(image_dups)
+        ncols = max([len(x) for x in image_dups])
+        #n_ims = len(image_list)
+        #ncols = 4  # fixed for a consistent layout
+        #nrows = int(np.ceil(n_ims / ncols)) + 1
+        fig = figure.Figure(figsize=(10, 14))
+
+        gs = gridspec.GridSpec(nrows=nrows, ncols=ncols)
+        ax = plt.subplot(
+            gs[0, 1:3]
+        )  # Always plot the original image in the middle of top row
+
+        for dup in image_dups:
+            id = dup[0]
+            fn = id_map[id]
+            ax.imshow(Image.open(fn))
+            #ax.imshow(Image.open(image_dir / orig))
+            #ax.set_title('Original Image: {}'.format(orig))
+            ax.axis('off')
+        gs.tight_layout(fig)
+        plt.show()
+        return
+        for i in range(0, n_ims):
+            row_num = (i // ncols) + 1
+            col_num = i % ncols
+
+            ax = plt.subplot(gs[row_num, col_num])
+            if scores:
+                ax.imshow(Image.open(image_dir / image_list[i][0]))
+                val = _formatter(image_list[i][1])
+                title = ' '.join([image_list[i][0], f'({val})'])
+            else:
+                ax.imshow(Image.open(image_dir / image_list[i]))
+                title = image_list[i]
+
+            ax.set_title(title, fontsize=6)
+            ax.axis('off')
+        gs.tight_layout(fig)
+
+        if outfile:
+            plt.savefig(outfile)
+
+        plt.show()
+
 
     def foo(self):
-        self.encode_dir('images-dedup', replace=True)
+        #self.encode_dir('images-dedup', replace=True)
+        images_dups, ids_seen = self.find_duplicates()
+        id_map = self.image_ids_2_filename(ids_seen)
+        self.plot_images(images_dups, id_map)
         return
         phasher = PHash()
 

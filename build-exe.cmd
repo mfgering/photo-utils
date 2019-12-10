f62ffaed63a050445d118d56ce74629b70a453cf
Mike Gering <mike@mfg-consulting-llc.com>
2019-12-10 12:34:45 -0500

    exe files

---
+++ build-exe.cmd
@@ -0,0 +1,4 @@
+pyinstaller --add-binary app-icon.jpg;. -i app-icon.ico -F --noconsole movedem_gui.py
+pyinstaller --add-binary app-icon.jpg;. -i app-icon.ico -F --noconsole movedem.py
+pyinstaller --add-binary app-icon.jpg;. -i app-icon.ico -F --noconsole phototags-gui.py
+pyinstaller --add-binary app-icon.jpg;. -i app-icon.ico -F --noconsole phototags.py
